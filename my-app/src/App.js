48b435ed3367ebc9ccdea100f898c4b09e0ed1e9
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2023-01-09 19:12:25 -0500

    Rendered a list of numbers

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,28 @@
-import "./App.css";
+import React from "react";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const Number = (props) => {
+    return <li>{props.value}</li>;
+  };
+
+  const NumberList = (props) => {
+    const numbers = props.numbers;
+    return (
+      <ul>
+        {numbers.map((item) => (
+          <Number key={`item ${item}`} value={item} />
+        ))}
+      </ul>
+    );
+  };
+
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  return (
+    <div className="App">
+      <NumberList numbers={numbers} />
+    </div>
+  );
+};
 
 export default App;
